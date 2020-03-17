8df47eac77661bdad97a8c0050c0ab235e6a068d
JAPM <cicelcup@gmail.com>
2020-03-16 21:22:34 -0600

    Add View Model to activity

--- app/src/main/java/com/example/testing_fragment_communication/MainActivity.kt
+++ app/src/main/java/com/example/testing_fragment_communication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.testing_fragment_communication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     private val firstFragment = FirstFragment()
     private val secondFragment = SecondFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,5 +21,7 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction2.replace(R.id.frame_two,secondFragment)
         fragmentTransaction2.commit()
 
+        val vm:SharedViewModel = ViewModelProvider(this)
+            .get(SharedViewModel::class.java)
     }
 }
