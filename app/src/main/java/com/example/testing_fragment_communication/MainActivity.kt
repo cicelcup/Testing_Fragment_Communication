0f17dfd8ebfaaaeb347dc2eb16608ae2620ac668
JAPM <cicelcup@gmail.com>
2020-03-17 06:44:45 -0600

    Communication with binding and observer

--- app/src/main/java/com/example/testing_fragment_communication/MainActivity.kt
+++ app/src/main/java/com/example/testing_fragment_communication/MainActivity.kt
@@ -5,22 +5,26 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+//    Definition of the fragments
     private val firstFragment = FirstFragment()
     private val secondFragment = SecondFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Adding first fragment to the activity
         val fragmentTransaction =
             supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_one,firstFragment)
         fragmentTransaction.commit()
 
+//        Adding second fragment to the activity
         val fragmentTransaction2 =
             supportFragmentManager.beginTransaction()
         fragmentTransaction2.replace(R.id.frame_two,secondFragment)
         fragmentTransaction2.commit()
 
+//        Adding View Model to the activity
         val vm:SharedViewModel = ViewModelProvider(this)
             .get(SharedViewModel::class.java)
     }
--- app/src/main/java/com/example/testing_fragment_communication/SharedViewModel.kt
+++ app/src/main/java/com/example/testing_fragment_communication/SharedViewModel.kt
@@ -4,7 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SharedViewModel: ViewModel() {
-    val _inputNumber = MutableLiveData<String>("Jorge")
-    val inputNumber:LiveData<String> = _inputNumber
+class SharedViewModel : ViewModel() {
+//    Variables with MutableLiveData (Used them for putting values
+//    into it and observe the value
+
+    //    Variable used it without binding
+    val _exampleText2 = MutableLiveData<String>("Jorge")
+
+    //    Variable used it with binding
+    val _exampleText = MutableLiveData<String>("Jorge")
+    val inputNumber: LiveData<String> = _exampleText
 }
